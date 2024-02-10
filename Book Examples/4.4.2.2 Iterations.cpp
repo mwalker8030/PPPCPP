ec25c27777953d893d5a7eb561645863e8d29319
mwalker8030 <michaelewalker8030@proton.me>
2024-02-10 11:22:26 -0800

    Try this exercise 4.4.2.2

---
+++ Book Examples/4.4.2.2 Iterations.cpp
@@ -0,0 +1,17 @@
+/**
+ * Rewrite the character value example from the previous Try this to use a 
+ * for-statement. Then modify your program to also write out a table of the integer 
+ * values for uppercase letters and digits.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    char ch = 'A';
+    for(int i = 0; ch + i <= 'z'; i++){
+        // something tells me this would cause a narrowing conversion error
+        cout << char(ch + i) << " " << int(ch + i) << endl;
+    }
+}
