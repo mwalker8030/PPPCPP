b3f056a58bdbcc5efbdeab2f088721cf342bea00
mwalker8030 <michaelewalker8030@proton.me>
2024-02-11 11:43:00 -0800

    finished the Chapter 4 drill 3

---
+++ Book Drills/Chapter 4 Drills 3.cpp
@@ -0,0 +1,73 @@
+/**
+ * Augment the program so that it writes the line the numbers are equal(only) if they are equal.
+*/
+
+#include <iostream>
+
+using namespace std;
+/**
+ * @brief gets the largest value from a vector of integers.
+ * @param v: A vector of integers
+ * @return: returns the largest number in the vector
+*/
+int getLarge(vector<int> v);
+/**
+ * @brief gets the smallest value from a vector of integers.
+ * @param v: A vector of integers
+ * @return: returns the smallest number in the vector
+*/
+int getSmall(vector<int> v);
+
+int main(){
+    vector<int> nums;
+    double temp;
+    while (cin>>temp){
+        if(temp == '|')
+            break;
+        nums.push_back(temp);
+    }
+
+    for(int i = 0; i < nums.size(); i++){
+        cout << nums[i] << " ";
+        if((i + 1) % 2 == 0){
+            if(nums[i-1] == nums[i]){
+                cout << " the numbers are equal";
+            }
+            cout << "\n";
+        }
+    }
+    sort(nums.begin(), nums.end());
+    cout << "\nThe smallest value is: " << getSmall(nums) << "\n";
+    cout << "The largest value is: " << getLarge(nums) << "\n";
+
+}
+
+/**
+ * @brief gets the largest value from a vector of integers.
+ * @param v: A vector of integers
+ * @return: returns the largest number in the vector
+ */
+int getLarge(vector<int> v){
+    int largest = v[0];
+    for(int i = 0; i < v.size(); i++){
+        if(v[i] > largest){
+            largest = v[i];
+        }
+    }
+    return largest;
+}
+
+/**
+ * @brief gets the smallest value from a vector of integers.
+ * @param v: A vector of integers
+ * @return: returns the smallest number in the vector
+*/
+int getSmall(vector<int> v){
+    int smallest = v[0];
+    for(int i = 0; i < v.size(); i++){
+        if(v[i] < smallest){
+            smallest = v[i];
+        }
+    }
+    return smallest;
+}
