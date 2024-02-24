8dc4d53604686e1a822acb0347e81fd3064643a2
mwalker8030 <michaelewalker8030@proton.me>
2024-02-24 12:50:25 -0800

    Finished Chapter 6 Exercise 9

--- Book Exercises/Chapter 6 Exercise 6.cpp
+++ Book Exercises/Chapter 6 Exercise 6.cpp
@@ -1,85 +1,41 @@
 /**
- * Redo the "Bulls and Cows" game from exercise 12 in chapter 5 to use four letters instead rather than four digits.
+ * 9. Write a program that reads digits and composes them into integers. For example, 123 is read as the characters 1, 2, and 3. The program should output 123 is 1 hundred and 2 ten and 3 ones. The number should be output as an int value. Handle numbers with one, two, three, or four digits. Hint: To get the integer value 5 from the character ‘5’ subtract ‘0’, that is, ’5’-‘0’ == 5.
 */
+
 #include <iostream>
+#include <cstdlib>
 #include <random>
 
 using namespace std;
 
 int randi();
-void fill(vector<char> &abc);
-bool check(vector<char> &abc, vector<char> &ans);
-bool count(vector<pair<bool, bool>> result);
-string plural(int n);
-vector<char> abc(4), ans(4);
-
+const int MAX = 9, MIN = 0;
+vector<char> numc;
+const vector<string> place = {"ones", "ten", "hundred", "thousand", "million", "billion", "trillion"};
+void describe_number(vector<char> &numc);
 int main(){
-    cout << "Welcome to the game of Bulls and Cows!" << endl;
-    fill(abc);
-    bool wrong = true;
-    cout << "Here are the four numbers you have to guess: " << endl;
-    while(wrong) {
-        cout << "Enter your guess: ";
-        for(int i = 0; i < 4; i++) {
-            cin >> ans[i];
-        }
-        wrong = check(abc, ans);
+    int n;
+    cout << "How many numbers do you want this value to have? ";
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        numc.push_back(randi() + '0');
     }
 
+    describe_number(numc);
+    return 0;
 }
 
 int randi(){
     static mt19937 mt(random_device{}());
-    static uniform_int_distribution<int> dist(0, 9);
+    static uniform_int_distribution<int> dist(MIN, MAX);
     return dist(mt);
 }
 
-void fill(vector<char> &abc){
-    for(int i = 0; i < 4; i++){
-        abc[i] = randi() + 'a';
+void describe_number(vector<char> &numc){
+    cout << "Then number ";
+    for(char c : numc){ cout << c;}
+    cout << " is ";
+    for(int i = 0; i < numc.size(); i++){
+        cout << numc[i] << " " << place[numc.size() - 1 - i] << " and ";
     }
-    #if 0
-    cout << abc[0] << " " << abc[1] << " " << abc[2] << " " << abc[3] << endl;
-    #endif
-}
-
-bool check(vector<char> &abc, vector<char> &ans){
-    vector<pair<bool, bool>> ans_check(4, pair(false,false));
-    for(int i = 0; i < 4; i++){
-        for(int j = 0; j < 4; j++){
-            if(abc[i] == ans[j]){
-                ans_check[i].first = true;
-                ans_check[i].second = (i == j);
-            }
-        }
-    }
-
-    return !count(ans_check);
-}
-
-bool count(vector<pair<bool, bool>> result)
-{
-    int bulls = 0, cows = 0;
-    for(int i = 0; i < 4; i++){
-        if(result[i].first && result[i].second){
-            bulls++;
-        } else if (result[i].first && !result[i].second) {
-            cows++;
-        }
-    }
-    cout << bulls << " Bull" << plural(bulls);;
-    cout << " and " << cows << " Cow" << plural(cows) << endl;
-    if(bulls == 4){
-        cout << "Congrats! You got all four digits correct in the correct order!" << endl;
-        return true;
-    }else {
-        fill(result.begin(), result.end(), pair(false, false));
-        return false;
-    }
-}
-
-string plural(int n)
-{
-     if(n > 1) return "s";
-    return string();
 }
